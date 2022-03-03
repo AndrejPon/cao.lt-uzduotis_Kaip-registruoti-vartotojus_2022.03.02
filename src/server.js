036cc6da576fc6c985fb63c56f317a606827be73
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-03-03 13:16:07 +0200

    post to db with hash works

--- src/server.js
+++ src/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const Joi = require('joi');
+const mysql = require('mysql2/promise');
+
+const dbConfig = require('./dbConfig');
 
 const PORT = process.env.SERVER_PORT || 3000;
-
+const userSchema = Joi.object({
+  username: Joi.string().required(),
+  password: Joi.string().required(),
+});
 const app = express();
 
 // middleware
@@ -12,8 +19,38 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-  res.json(users);
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.post('/register', async (req, res) => {
+  let userData = req.body;
+  try {
+    userData = await userSchema.validateAsync(userData);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ error: 'Incorrect data sent' });
+  }
+  try {
+    const hashedPassword = bcrypt.hashSync(userData.password, 10);
+    const conn = await mysql.createConnection(dbConfig);
+    const sql = `
+    INSERT INTO users (username, password)
+    VALUES (?, ?)
+    `;
+    const [newUserdata] = await conn.execute(sql, [
+      userData.username,
+      hashedPassword,
+    ]);
+
+    await conn.close();
+    return res.send(newUserdata);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ error: 'Unexpected error. Please try again.' });
+  }
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
